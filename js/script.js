062878a6d3992c29ea2352da61e2829b5b174d00
Fairdebt <fairdebt2@gmail.com>
2025-02-14 12:22:35 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -137,5 +137,19 @@ document.addEventListener("DOMContentLoaded", function () {
       alert("Form submitted successfully!"); // Replace this with actual form submission logic
       document.getElementById("contactForm").reset(); // Clear form after submission
     }
+    
+  });
+});
+
+document.querySelectorAll('.read-more-btn').forEach(button => {
+  button.addEventListener('click', function() {
+      const content = this.nextElementSibling;
+      if (content.style.display === 'block') {
+          content.style.display = 'none';
+          this.textContent = 'Read More';
+      } else {
+          content.style.display = 'block';
+          this.textContent = 'Read Less';
+      }
   });
 });
